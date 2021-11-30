2dc6747782522cac0be18f901d98f27352587294
hovanhue2k1@gmail.com <hovanhue2k1@gmail.com>
2021-11-30 21:21:50 +0700

    up bio

---
+++ script.js
@@ -0,0 +1,9 @@
+const phone = document.querySelector('#phone');
+phone.addEventListener('click', () =>{
+    alert('0368139853')
+});
+
+const wc = document.querySelector('#wc');
+wc.addEventListener('click', () => {
+    alert('điện thoại hư roài!')
+})
